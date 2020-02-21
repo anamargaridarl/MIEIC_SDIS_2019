84ebc7f1ab1001dd9edf301e2543eb578de5e90f
Ana Margarida <anamargaridarl@gmail.com>
2020-02-21 17:37:11 +0000

    need testing

--- lab1/src/Client.java
+++ lab1/src/Client.java
@@ -8,7 +8,8 @@ public class Client {
     private static int port;
     private static InetAddress address;
     private static DatagramSocket socket = null;
-
+    private static DatagramPacket packet;
+    private static byte[] buf;
 
     public static void main(String[] args) throws IOException {
         port = Integer.parseInt(args[1]);
@@ -16,16 +17,35 @@ public class Client {
         socket = new DatagramSocket();
 
         sendRequest();
-        processReply();
+        processReply(args);
     }
 
-    private static void processReply() {
-        System.out.print("Hola");
+    private static void processReply(String[] operands) throws IOException {
+
+            buf =  new byte[256];
+            socket.receive(packet);
+            buf = packet.getData();
+
+            String[] result = new String(buf,buf.length).split(" ");
+            System.out.print("Client: " );
+
+            for(int i = 2; i < operands.length; i++) {
+                System.out.print(" " + operands[i]);
+            }
+
+            System.out.print(":" );
+
+            for(String r: result) {
+                System.out.print(" " + r);
+            }
+            
+            System.out.print("\n");
+
     }
 
     private static void sendRequest() throws IOException {
-        byte[] buf = new byte[256];
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
+        buf =  new byte[256];
+        packet = new DatagramPacket(buf, buf.length, address, port);
         socket.send(packet);
     }
 
--- lab1/src/ServerThread.java
+++ lab1/src/ServerThread.java
@@ -2,16 +2,15 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.lang.String;
-import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ServerThread {
     DatagramSocket socket;
-    Map<InetAddress,String> dns_db;
+    Map<String,String> dns_db;
     ServerThread(String port) throws IOException {
         socket = new DatagramSocket(Integer.parseInt(port));
-        dns_db = new HashMap<InetAddress, String>();
+        dns_db = new HashMap<String, String>();
     }
 
     void start() throws IOException {
@@ -20,7 +19,7 @@ public class ServerThread {
         while(true) {
             packet = new DatagramPacket(data,data.length);
             socket.receive(packet);
-            data = processPacket(packet).getBytes();
+            data =  processPacket(packet).getBytes();
             reply(packet,data);
 
         }
@@ -32,13 +31,26 @@ public class ServerThread {
     private String processPacket(DatagramPacket packet) {
         byte[] data = packet.getData();
         String[] args = new String(data,data.length).split(" ");
-        String response = "-1";
 
+        String response = "-1";
         logRequest(args);
-        /*if(args[0].equalsIgnoreCase("REGISTER"))
-            response = registerAddress(args[1],args[2]);
-        else if(args[0].equalsIgnoreCase("LOOKUP"))
-            response = lookupAddress(args[1]);*/
+
+        if(args[0].equalsIgnoreCase("REGISTER")) {
+            try {
+                int value = registerAddress(args[1],args[2]);
+                return Integer.toString(value);
+            } catch (AlreadyRegistered alreadyRegistered) {
+                return response;
+            }
+        }
+
+        else if(args[0].equalsIgnoreCase("LOOKUP")) {
+            try {
+                return lookupAddress(args[1]);
+            } catch (NoAddress noAddress) {
+                return response;
+            }
+        }
 
         return response;
     }
@@ -51,11 +63,27 @@ public class ServerThread {
         System.out.print("\n");
     }
 
-    /*
-    private String registerAddress(String arg, String arg1) {
 
+    private int registerAddress(String dns, String ip) throws AlreadyRegistered {
+        if(dns_db.containsKey(dns))
+            throw new AlreadyRegistered();
+        else {
+            dns_db.put(dns, ip);
+            return dns_db.size();
+        }
     }
 
-    private String lookupAddress(String arg) {
-    }*/
+    private String lookupAddress(String dns) throws NoAddress {
+
+        String response;
+
+        if(dns_db.containsKey(dns)) {
+            response = dns + dns_db.get(dns);
+        }
+        else
+            throw new NoAddress();
+
+        return response;
+
+    }
 }
